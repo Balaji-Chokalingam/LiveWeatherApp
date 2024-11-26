477bd71184049675b11e87c93f894db9843c4863
Balaji-Chokalingam <163843437+Balaji-Chokalingam@users.noreply.github.com>
2024-11-26 19:38:35 +0530

    Update LiveWeatherapp.py

--- LiveWeatherapp.py
+++ LiveWeatherapp.py
@@ -3,7 +3,6 @@ import tkinter as tk
 import requests
 from tkinter import messagebox
 from PIL import Image, ImageTk
-import ttkbootstrap
 
 #Function to get weather information from OpenWeatherMap Api
 def get_weather(city):
